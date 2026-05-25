ca2042088eae13eb7b5c2561b3d27257ffe0d9ce
Poonguzhali_Tamilmaran <poonguzhali.techietact@gmail.com>
2026-05-26 01:17:04 +0530

    Integrated Angular dashboard with Spring Boot backend APIs
    Implemented full-stack dashboard integration between Angular frontend and Spring Boot backend.
    
    Frontend Enhancements:
    - Created Dashboard module
    - Integrated PrimeNG DataTable
    - Added responsive dashboard layout
    - Implemented logout functionality
    - Added role-based status tags
    - Created UserService for API communication
    - Integrated Angular HttpClient
    - Implemented Observable-based API handling
    
    Backend Integration:
    - Consumed GET /api/users API
    - Displayed real MariaDB user records dynamically
    - Connected Angular frontend with Spring Boot backend
    
    Architecture Improvements:
    - Added reusable User model
    - Implemented service-based frontend architecture
    - Improved UI structure and component separation
    
    Verified:
    - Real-time backend data fetching
    - Dynamic table rendering
    - Successful frontend-backend integration

---
+++ angular/src/app/models/user-record.ts
@@ -0,0 +1,6 @@
+export interface UserRecord {
+    id: number;
+    name: string;
+    role: string;
+    status: string;
+}
--- angular/src/app/pages/dashboard/dashboard.ts
+++ angular/src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    ButtonModule,
+    CommonModule,
+    TableModule,
+    TagModule
+  ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
 export class Dashboard {
+  
+  users: User[] = [];
+  
+  constructor( private router: Router, private userService: UserService ) {}
 
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe({
+        next: (response) => {
+          this.users = response;
+          console.log(response);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'Active':
+        return 'success';
+      case 'Inactive':
+        return 'danger';
+      default:
+        return 'info';
+    }
+  }
 }
