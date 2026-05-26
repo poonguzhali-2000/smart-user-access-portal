62763d082f6b4ec384fdebb12b2046453d142c92
Poonguzhali_Tamilmaran <poonguzhali.techietact@gmail.com>
2026-05-26 22:39:28 +0530

    Implemented Angular JWT interceptor and protected routing
    Implemented Angular route protection and JWT interceptor for secured frontend navigation and authenticated API communication.
    
    Features:
    - Route Guard using CanActivate
    - Protected dashboard routing
    - Automatic JWT Bearer token injection
    - Centralized authentication handling
    - Secure frontend navigation
    
    Verified:
    - Unauthorized dashboard access blocked
    - JWT automatically attached to API requests

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -4,14 +4,19 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt-interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([
+        jwtInterceptor
+      ])
+    ),
     providePrimeNG({
       theme: {
         preset: Aura
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Login },
-    { path: 'dashboard', component: Dashboard }
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] }
 ];
